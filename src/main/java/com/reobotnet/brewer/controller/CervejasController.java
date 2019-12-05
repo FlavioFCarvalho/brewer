ea0bbb02818155c676dc0530fc6321f3a9481824
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-05 02:49:36 -0300

    Começando o cadastro de cerveja

--- src/main/java/com/reobotnet/brewer/controller/CervejasController.java
+++ src/main/java/com/reobotnet/brewer/controller/CervejasController.java
@@ -2,11 +2,20 @@ package com.reobotnet.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.reobotnet.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println(">>> sku: " + cerveja.getSku());
 		return "cerveja/CadastroCerveja";
 	}
 }
---
+++ src/main/java/com/reobotnet/brewer/model/Cerveja.java
@@ -0,0 +1,34 @@
+package com.reobotnet.brewer.model;
+
+public class Cerveja {
+	
+	private String sku;
+	private String nome;
+	
+	public Cerveja() {
+		
+	}
+	
+	
+	public Cerveja(String sku, String nome) {
+		this.sku = sku;
+		this.nome = nome;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
