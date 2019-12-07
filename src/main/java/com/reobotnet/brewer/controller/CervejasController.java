ffd3f04355ae9697d5c9becdd8420c41251d7102
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-07 01:10:00 -0300

    Forward e Redirect

--- src/main/java/com/reobotnet/brewer/controller/CervejasController.java
+++ src/main/java/com/reobotnet/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.reobotnet.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.reobotnet.brewer.model.Cerveja;
 
@@ -17,11 +19,15 @@ public class CervejasController {
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
-		if(result.hasErrors()) {
-			System.out.println("Tem erro sim !");
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
 		}
+		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>> sku: " + cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		return "redirect:/cervejas/novo";
 	}
 }
