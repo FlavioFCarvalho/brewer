ab3d3c26a406d7230b7401bea0491a51ba165952
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-07 02:15:35 -0300

    Exibindo as msgs de erro de cada campo e mantendo os dados no formulário.

--- src/main/java/com/reobotnet/brewer/controller/CervejasController.java
+++ src/main/java/com/reobotnet/brewer/controller/CervejasController.java
@@ -14,15 +14,14 @@ import com.reobotnet.brewer.model.Cerveja;
 @Controller
 public class CervejasController {
 	@RequestMapping("/cervejas/novo")
-	public String novo() {
+	public String novo(Cerveja cerveja) {
 		return "cerveja/CadastroCerveja";
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
 	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			model.addAttribute("mensagem", "Erro no formulário");
-			return "cerveja/CadastroCerveja";
+			return novo(cerveja);
 		}
 		
 		// Salvar no banco de dados...
