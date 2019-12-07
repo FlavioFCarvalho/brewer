246e89caa53ae33ffcec31d02c96345c68c5f5df
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-07 01:16:49 -0300

    Adicionando o nome e a descrição da cerveja , e algumas validações.

--- src/main/java/com/reobotnet/brewer/model/Cerveja.java
+++ src/main/java/com/reobotnet/brewer/model/Cerveja.java
@@ -1,21 +1,28 @@
 package com.reobotnet.brewer.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	
 	@NotBlank
 	private String sku;
+	@NotBlank
 	private String nome;
+	
+	@Size(min = 1, max = 50)
+	private String descricao;
 	
 	public Cerveja() {
 		
 	}
 	
 	
-	public Cerveja(String sku, String nome) {
+	public Cerveja(String sku, String nome, String descricao) {
 		this.sku = sku;
 		this.nome = nome;
+		this.descricao = descricao;
 	}
 
 	public String getSku() {
@@ -33,5 +40,17 @@ public class Cerveja {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 
 }
