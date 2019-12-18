84fa2b4f3f02acd8d42568dfd8338c4b08c14646
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-18 10:29:08 -0300

    Removendo o teste Cadastro_produto.html

--- src/main/java/com/reobotnet/brewer/controller/CervejasController.java
+++ src/main/java/com/reobotnet/brewer/controller/CervejasController.java
@@ -30,8 +30,5 @@ public class CervejasController {
 		return "redirect:/cervejas/novo";
 	}
 	
-	@RequestMapping("/cervejas/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
+	
 }
