90586eb5810bf0c060f5b71f5a299245a4ee1787
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-18 14:27:28 -0300

    Template do layout.

--- src/main/java/com/reobotnet/brewer/config/WebConfig.java
+++ src/main/java/com/reobotnet/brewer/config/WebConfig.java
@@ -19,6 +19,8 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.reobotnet.brewer.controller.CervejasController;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 @Configuration
 @ComponentScan(basePackageClasses = { CervejasController.class })
 @EnableWebMvc
@@ -44,6 +46,8 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+		
+		engine.addDialect(new LayoutDialect());
 		return engine;
 	}
 
