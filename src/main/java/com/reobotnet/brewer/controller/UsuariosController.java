fecbb8fb7e0c02de4bbf393e31b14204f56b7b11
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-19 12:59:35 -0300

    Prototipando o cadastro de usuário

---
+++ src/main/java/com/reobotnet/brewer/controller/UsuariosController.java
@@ -0,0 +1,13 @@
+package com.reobotnet.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UsuariosController {
+	@RequestMapping("/usuarios/novo")
+	public String novo() {
+		return "usuario/CadastroUsuario";
+	}
+	
+}
