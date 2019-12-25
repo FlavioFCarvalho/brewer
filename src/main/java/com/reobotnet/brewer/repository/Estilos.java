9294e6d18b030e18345e84d092444aca8af2db81
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-25 19:32:12 -0300

    Validando se existe um estilo persistido

--- src/main/java/com/reobotnet/brewer/repository/Estilos.java
+++ src/main/java/com/reobotnet/brewer/repository/Estilos.java
@@ -1,5 +1,7 @@
 package com.reobotnet.brewer.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.reobotnet.brewer.model.Estilo;
 @Repository
 public interface Estilos extends JpaRepository<Estilo, Long> {
 
-	
+	public Optional<Estilo> findByNomeIgnoreCase(String nome);
 }
--- src/main/java/com/reobotnet/brewer/service/CadastroEstiloService.java
+++ src/main/java/com/reobotnet/brewer/service/CadastroEstiloService.java
@@ -1,20 +1,29 @@
 package com.reobotnet.brewer.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.reobotnet.brewer.model.Estilo;
 import com.reobotnet.brewer.repository.Estilos;
+import com.reobotnet.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Service
 public class CadastroEstiloService {
-	
+
 	@Autowired
 	private Estilos estilos;
 	
 	@Transactional
 	public void salvar(Estilo estilo) {
+		Optional<Estilo> estiloOptional = estilos.findByNomeIgnoreCase(estilo.getNome());
+		if (estiloOptional.isPresent()) {
+			throw new NomeEstiloJaCadastradoException("Nome do estilo já cadastrado");
+		}
+		
 		estilos.save(estilo);
 	}
+	
 }
