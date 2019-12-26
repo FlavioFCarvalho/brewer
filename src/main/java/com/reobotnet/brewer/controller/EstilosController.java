fd2f4522663d5c153904c4ee32388acbadfd64c3
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-26 11:24:08 -0300

    Salvando o estilo com Ajax: diálogo de cadastro rápido.

--- src/main/java/com/reobotnet/brewer/controller/EstilosController.java
+++ src/main/java/com/reobotnet/brewer/controller/EstilosController.java
@@ -3,10 +3,14 @@ package com.reobotnet.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,7 +20,7 @@ import com.reobotnet.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Controller
 public class EstilosController {
-	
+
 	@Autowired
 	private CadastroEstiloService cadastroEstiloService;
 	
@@ -41,4 +45,20 @@ public class EstilosController {
 		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
 		return new ModelAndView("redirect:/estilos/novo");
 	}
+	
+	@RequestMapping(value = "/estilos", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result) {
+		if (result.hasErrors()) {
+			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
+		}
+		
+		try {
+			estilo = cadastroEstiloService.salvar(estilo);
+		} catch (NomeEstiloJaCadastradoException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+		return ResponseEntity.ok(estilo);
+	}
+	
 }
