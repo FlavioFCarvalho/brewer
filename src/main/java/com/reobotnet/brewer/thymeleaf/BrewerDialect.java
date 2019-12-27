0ecd6c153946f91d3e840d37a62e876b1f11bc4b
Flávio Carvalho <flaviocfr@gmail.com>
2019-12-27 11:50:15 -0300

    Novo elemento para mensagens.

--- src/main/java/com/reobotnet/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/reobotnet/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import com.reobotnet.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import com.reobotnet.brewer.thymeleaf.processor.MessageElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect {
 
@@ -19,6 +20,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
 	public Set<IProcessor> getProcessors(String dialectPrefix) {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
+		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
