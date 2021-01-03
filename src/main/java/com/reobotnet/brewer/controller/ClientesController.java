0e619ec418c8a37103141b675a0a34539e076671
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-02 22:12:50 -0300

    Carregando o estado no cadastro de cliente.

--- src/main/java/com/reobotnet/brewer/controller/ClientesController.java
+++ src/main/java/com/reobotnet/brewer/controller/ClientesController.java
@@ -1,19 +1,25 @@
 package com.reobotnet.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.reobotnet.brewer.model.TipoPessoa;
+import com.reobotnet.brewer.repository.Estados;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
+	
+	@Autowired
+	private Estados estados;
 
 	@RequestMapping("novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
 	
