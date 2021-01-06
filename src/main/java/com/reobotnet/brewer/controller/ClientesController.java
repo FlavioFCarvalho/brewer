829cc96f881e0b6bef2ae3c7a541b4c5fc258a42
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-06 19:14:50 -0300

    Iniciando a pesquisa do cliente

--- src/main/java/com/reobotnet/brewer/controller/ClientesController.java
+++ src/main/java/com/reobotnet/brewer/controller/ClientesController.java
@@ -1,18 +1,25 @@
 package com.reobotnet.brewer.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.reobotnet.brewer.controller.page.PageWrapper;
 import com.reobotnet.brewer.model.Cliente;
 import com.reobotnet.brewer.model.TipoPessoa;
+import com.reobotnet.brewer.repository.Clientes;
 import com.reobotnet.brewer.repository.Estados;
+import com.reobotnet.brewer.repository.filter.ClienteFilter;
 import com.reobotnet.brewer.service.CadastroClienteService;
 import com.reobotnet.brewer.service.exception.CpfCnpjClienteJaCadastradoException;
 
@@ -26,6 +33,9 @@ public class ClientesController {
 	
 	@Autowired
 	private CadastroClienteService cadastroClienteService;
+	
+	@Autowired
+	private Clientes clientes;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo(Cliente cliente) {
@@ -50,6 +60,18 @@ public class ClientesController {
 		
 		attributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
 		return new ModelAndView("redirect:/clientes/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(ClienteFilter clienteFilter, BindingResult result
+			, @PageableDefault(size = 3) Pageable pageable, HttpServletRequest httpServletRequest) {
+		ModelAndView mv = new ModelAndView("cliente/PesquisaClientes");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		
+		PageWrapper<Cliente> paginaWrapper = new PageWrapper<>(clientes.filtrar(clienteFilter, pageable)
+				, httpServletRequest);
+		mv.addObject("pagina", paginaWrapper);
+		return mv;
 	}
 	
 }
--- src/main/java/com/reobotnet/brewer/repository/Clientes.java
+++ src/main/java/com/reobotnet/brewer/repository/Clientes.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reobotnet.brewer.model.Cliente;
+import com.reobotnet.brewer.repository.helper.cliente.ClientesQueries;
 
-public interface Clientes extends JpaRepository<Cliente, Long> {
+public interface Clientes extends JpaRepository<Cliente, Long>, ClientesQueries {
 
 	public Optional<Cliente> findByCpfOuCnpj(String cpfOuCnpj);
 
