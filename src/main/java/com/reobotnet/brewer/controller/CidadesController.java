bfb5f4a62912577908e170446700b7a4d46efd78
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-08 19:13:58 -0300

    Cadastro de Cidade.

--- src/main/java/com/reobotnet/brewer/controller/CidadesController.java
+++ src/main/java/com/reobotnet/brewer/controller/CidadesController.java
@@ -2,17 +2,24 @@ package com.reobotnet.brewer.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.reobotnet.brewer.model.Cidade;
 import com.reobotnet.brewer.repository.Cidades;
-
-
+import com.reobotnet.brewer.repository.Estados;
+import com.reobotnet.brewer.service.CadastroCidadeService;
+import com.reobotnet.brewer.service.exception.NomeCidadeJaCadastradaException;
 
 @Controller
 @RequestMapping("/cidades")
@@ -21,9 +28,17 @@ public class CidadesController {
 	@Autowired
 	private Cidades cidades;
 	
+	@Autowired
+	private Estados estados;
+	
+	@Autowired
+	private CadastroCidadeService cadastroCidadeService;
+	
 	@RequestMapping("/nova")
-	public String nova() {
-		return "cidade/CadastroCidade";
+	public ModelAndView nova(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("cidade/CadastroCidade");
+		mv.addObject("estados", estados.findAll());
+		return mv;
 	}
 	
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -33,6 +48,23 @@ public class CidadesController {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {	}
 		return cidades.findByEstadoCodigo(codigoEstado);
+	}
+	
+	@PostMapping("/nova")
+	public ModelAndView salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return nova(cidade);
+		}
+		
+		try {
+			cadastroCidadeService.salvar(cidade);
+		} catch (NomeCidadeJaCadastradaException e) {
+			result.rejectValue("nome", e.getMessage(), e.getMessage());
+			return nova(cidade);
+		}
+		
+		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
+		return new ModelAndView("redirect:/cidades/nova");
 	}
 	
 }
--- src/main/java/com/reobotnet/brewer/repository/Cidades.java
+++ src/main/java/com/reobotnet/brewer/repository/Cidades.java
@@ -1,15 +1,17 @@
 package com.reobotnet.brewer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reobotnet.brewer.model.Cidade;
-
-
+import com.reobotnet.brewer.model.Estado;
 
 public interface Cidades extends JpaRepository<Cidade, Long> {
 
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+
+	public Optional<Cidade> findByNomeAndEstado(String nome, Estado estado);
 	
 }
