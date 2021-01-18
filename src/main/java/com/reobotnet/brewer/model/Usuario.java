f4f42fa2484992d76aa0619ed2e3b1b66be7715a
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-18 16:45:27 -0300

    Salvando o status do usuário

--- src/main/java/com/reobotnet/brewer/model/Usuario.java
+++ src/main/java/com/reobotnet/brewer/model/Usuario.java
@@ -14,6 +14,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
