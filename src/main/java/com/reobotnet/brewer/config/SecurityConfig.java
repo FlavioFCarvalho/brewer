9ec734eb11f18d485bc1cc89ea15b926e5c91654
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-27 18:13:32 -0300

    Tela de login customizada

--- src/main/java/com/reobotnet/brewer/config/SecurityConfig.java
+++ src/main/java/com/reobotnet/brewer/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.reobotnet.brewer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,12 +19,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");
+	}
+	
+	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
+				.loginPage("/login")
+				.permitAll()
 				.and()
 			.csrf().disable();
 	}
@@ -34,4 +44,3 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 }
-
