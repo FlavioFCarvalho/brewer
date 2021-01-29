ca3738a84aace34ed3f549f4cb2c1a723c4952af
FlavioFCarvalho <flaviocfr@gmail.com>
2021-01-29 12:32:35 -0300

    Restringindo o acesso às páginas

--- src/main/java/com/reobotnet/brewer/config/SecurityConfig.java
+++ src/main/java/com/reobotnet/brewer/config/SecurityConfig.java
@@ -37,6 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+				.antMatchers("/cidades/nova").hasRole("CADASTRAR_CIDADE")
+				.antMatchers("/usuarios/**").hasRole("CADASTRAR_USUARIO")
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
