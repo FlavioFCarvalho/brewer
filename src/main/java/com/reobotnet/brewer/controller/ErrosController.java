67b182687f22029d03f60b1fd27fa0a177dbd639
FlavioFCarvalho <flaviocfr@gmail.com>
2021-02-06 23:29:56 -0300

    Página 404 - Não encontrado

---
+++ src/main/java/com/reobotnet/brewer/controller/ErrosController.java
@@ -0,0 +1,14 @@
+package com.reobotnet.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrosController {
+
+	@GetMapping("/404")
+	public String paginaNaoEncontrada() {
+		return "404";
+	}
+	
+}
